e29d42249b1e479281419a402e3aaaf06b94e23c
karthikchittumuri <karthik.chittumuri@madtin.com>
2026-06-25 03:38:03 +0530

    use github_analyzer database

--- src/config/db.js
+++ src/config/db.js
@@ -6,7 +6,7 @@ const connectionConfig = {
   port: parseInt(process.env.DB_PORT) || 4000,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME || 'sys',
+  database: process.env.DB_NAME || 'github_analyzer',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
